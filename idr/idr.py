2a5e5754be18e8e3574a05683c555fedc0b76eca
Nathan Boley <npboley@gmail.com>
2015-01-26 21:54:52 -0800

    Modified the filtering technique to be more smooth

--- idr/idr.py
+++ idr/idr.py
@@ -162,7 +162,8 @@ def calc_IDR(theta, r1, r2):
     z1 = compute_pseudo_values(r1, mu, sigma, p, EPS=1e-12)
     z2 = compute_pseudo_values(r2, mu, sigma, p, EPS=1e-12)
     localIDR = 1-calc_post_membership_prbs(numpy.array(theta), z1, z2)
-    localIDR[(z1 < 0) & (z2 < 0)] = 1.
+    if FILTER_PEAKS_BELOW_NOISE_MEAN:
+        localIDR[z1 + z2 < 0] = (1-p) + p*localIDR[z1 + z2 < 0]
     local_idr_order = localIDR.argsort()
     ordered_local_idr = localIDR[local_idr_order]
     ordered_local_idr_ranks = rankdata( ordered_local_idr, method='max' )
